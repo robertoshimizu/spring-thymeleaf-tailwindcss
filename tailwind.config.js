a7b7d0ee63e75b23dd33efdee983665826345a49
Roberto Shimizu <rshimizubr@gmail.com>
2022-06-04 12:07:54 -0300

    feat: starter using gulp to postcss tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/main/resources/templates/**/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        'taming-thymeleaf-green':'#166538'
+      }
+    },
+  },
+  plugins: [],
+}
