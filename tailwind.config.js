f053c9d762b5f8f610022c7bcd54ca8bcc3b150b
Roberto Shimizu <rshimizubr@gmail.com>
2022-06-12 20:11:26 -0300

    chore: install tailwind forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ['./src/main/resources/templates/**/*.html'],
   theme: {
     extend: {
+      fontFamily:{
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
       colors: {
         'taming-thymeleaf-green':'#166538'
       }
     },
   },
-  plugins: [],
+  plugins: [
+      require('@tailwindcss/forms')
+  ],
 }
